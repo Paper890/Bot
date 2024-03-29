82c8b7441955c1ee7eba625f028c2d725f29e632
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-03-29 21:12:30 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install python3 python3-pip
+pip3 install python-telegram-bot
+#pip3 install python-telegram-bot==12.0.0 // Install jika python3 eror
 sudo apt install git -y
 cd
 mkdir bot
 cd bot
+touch ssh.txt
+touch vmess.txt
+touch vless.txt
+touch trojan.txt
+git clone https://github.com/Paper890/Bot.git
 pip install -r requirements.txt
