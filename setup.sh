815c030539ad2840cbbf54fe4acfee0e92a7357d
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-03-30 20:03:44 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
+# Warna teks
+red="\e[31m"
+green="\e[32m"
+yellow="\e[33m"
+blue="\e[34m"
+purple="\e[35m"
+cyan="\e[36m"
+NC="\e[0m"
+
 clear
-echo -e " ╔═╗┌─┐┌┐┌╔╗ ┌─┐┌┬┐"
-echo -e " ╚═╗├─┤│││╠╩╗│ │ │ "
-echo -e " ╚═╝┴ ┴┘└┘╚═╝└─┘ ┴ "
-echo -e "------Bot By san------"
+echo -e "
+░██████╗░█████╗░███╗░░██╗░░░██████╗░░█████╗░████████╗
+██╔════╝██╔══██╗████╗░██║░░░██╔══██╗██╔══██╗╚══██╔══╝
+╚█████╗░███████║██╔██╗██║░░░██████╦╝██║░░██║░░░██║░░░
+░╚═══██╗██╔══██║██║╚████║░░░██╔══██╗██║░░██║░░░██║░░░
+██████╔╝██║░░██║██║░╚███║██╗██████╦╝╚█████╔╝░░░██║░░░
+╚═════╝░╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═════╝░░╚════╝░░░░╚═╝░░░"
 sleep 3
 
+echo -e "${green}UPDATE PACKAGE VPS${NC}"
+sleep 2
 clear
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install python3 python3-pip
-#pip3 install python-telegram-bot==12.0.0 // Install jika python3 eror
 sudo apt install git -y
+echo -e "${yellow} UPDATE SELESAI ${NC}"
 
 #Buat folder Bot
 mkdir -p san/bot
@@ -25,3 +39,19 @@ git clone https://github.com/Paper890/Bot.git
 cd
 cd san/bot/Bot
 pip install -r requirements.txt
+
+#Fungsi jalankan Bot di background
+echo "[Unit]
+Description=Bot Service
+After=network.target
+[Service]
+User=root
+WorkingDirectory=/root/san/bot/Bot
+ExecStart=/usr/bin/python3 main.py
+Restart=always
+[Install]
+WantedBy=multi-user.target" >> /etc/systemd/system/bot.service
+
+sudo systemctl daemon-reload
+sudo systemctl start bot
+sudo systemctl enable bot
