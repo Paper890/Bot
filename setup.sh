2baaa2061b2f92a9d77fc7883e06c0c660e305d6
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-03-30 15:57:32 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,11 +7,11 @@ echo -e "------Bot By san------"
 sleep 3
 
 clear
-#sudo apt update -y
-#sudo apt upgrade -y
-#sudo apt install python3 python3-pip
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install python3 python3-pip
 #pip3 install python-telegram-bot==12.0.0 // Install jika python3 eror
-#sudo apt install git -y
+sudo apt install git -y
 
 #Buat folder Bot
 mkdir -p san/bot
