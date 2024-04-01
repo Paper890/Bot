9483072f3c446b79f41cd7a4c66230297748b4ec
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-04-01 09:13:32 +0800

    BACKUP MAIN

--- Co.py
+++ Co.py
@@ -1,55 +1,48 @@
 import telegram
-from telegram import ReplyKeyboardMarkup, KeyboardButton
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Updater, MessageHandler, Filters, CallbackQueryHandler
 
-# Fungsi untuk menangani pesan "/start"
-def start_command(update, context):
-    keyboard = [
-        [KeyboardButton('/start')],
-        [KeyboardButton('Halo')],
-        [KeyboardButton('1'), KeyboardButton('2')],
-        [KeyboardButton('3'), KeyboardButton('4')]
-    ]
-    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Halo! Pilih salah satu opsi berikut:', reply_markup=reply_markup)
+# Fungsi untuk membaca jawaban dari file .txt
+def read_answer_from_file(file_path):
+    with open(file_path, 'r') as file:
+        answer = file.read()
+    return answer
 
-# Fungsi untuk menangani pesan yang diterima
+# Fungsi untuk menangani pesan yang diterima oleh bot
 def handle_message(update, context):
-    message = update.message.text
-    response = get_response(message)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+    message = update.message.text.lower()
+    if message == "menu":
+        # Membuat keyboard inline
+        keyboard = [[InlineKeyboardButton("SSH", callback_data='sanvpn'),
+                     InlineKeyboardButton("VMESS", callback_data='vmess'),
+                     InlineKeyboardButton("VLESS", callback_data='vless'),
+                     InlineKeyboardButton("TROJAN", callback_data='trojan')]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        # Mengirim pesan dengan keyboard inline
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Halo selamat datang, silahkan pilih Paket nya di bawah",
+                                 reply_markup=reply_markup)
+# Fungsi untuk menangani tombol yang ditekan dalam keyboard inline
+def handle_button(update, context):
+    query = update.callback_query
+    if query.data == 'sanvpn':
+        answer = read_answer_from_file('sanvpn.txt')
+        context.bot.send_message(chat_id=query.message.chat_id, text=answer)
+    elif query.data == 'vmess':
+        answer = read_answer_from_file('vmess.txt')
+        context.bot.send_message(chat_id=query.message.chat_id, text=answer)
+    elif query.data == 'vless':
+        answer = read_answer_from_file('vless.txt')
+        context.bot.send_message(chat_id=query.message.chat_id, text=answer)
+    elif query.data == 'trojan':
+        answer = read_answer_from_file('trojan.txt')
+        context.bot.send_message(chat_id=query.message.chat_id, text=answer)
+# Inisialisasi bot dan menambahkan handler untuk pesan dan tombol
 
-# Fungsi untuk mendapatkan jawaban dari pertanyaan atau opsi button
-def get_response(message):
-    if message == 'Halo':
-        return 'Apa kabar?'
-    elif message == '1':
-        return 'Anda memilih opsi 1'
-    elif message == '2':
-        return 'Anda memilih opsi 2'
-    elif message == '3':
-        return 'Anda memilih opsi 3'
-    elif message == '4':
-        return 'Anda memilih opsi 4'
-    else:
-        return 'Maaf, saya tidak dapat memahami pertanyaan atau opsi Anda.'
-
-# Inisialisasi bot
-bot_token = '7090457834:AAGOVODwtJrA0II6-B8tfUGrSwJAdnbl1QY'
-bot = telegram.Bot(token=bot_token)
-
-# Inisialisasi updater dan dispatcher
-updater = Updater(bot_token, use_context=True)
-dispatcher = updater.dispatcher
-
-# Menambahkan handler untuk perintah "/start"
-start_handler = CommandHandler('start', start_command)
-dispatcher.add_handler(start_handler)
-
-# Menambahkan handler untuk menangani pesan yang diterima
-message_handler = MessageHandler(Filters.text & (~Filters.command), handle_message)
-dispatcher.add_handler(message_handler)
-
+bot = telegram.Bot(token='TOKEN') 
+updater = Updater(bot=bot, use_context=True)
+updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+updater.dispatcher.add_handler(CallbackQueryHandler(handle_button))
 # Memulai bot
 updater.start_polling()
 updater.idle()
